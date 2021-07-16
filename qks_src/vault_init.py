142b8dd60860a13e1a2186c1b96e0fcd0df3dcd9
LorenzoPintore <lorenzo.pintore1@gmail.com>
2021-07-16 19:17:15 +0200

    vault init debug function, dockercompose ready for tests

--- qks_src/vault_init.py
+++ qks_src/vault_init.py
@@ -1,11 +1,12 @@
-from asyncVaultClient import vaultClient 
+from asyncVaultClient import VaultClient 
 import asyncio
 
 async def init(): 
-    client = vaultClient.VaultClient("localhost", 8200, token=None)
-    print("init: ", client.initialize(1,1) )
-    keys = None # ['yKnbw2WKemkdvrhZGkobVIS4EGXDc6t7I+rbLMaV2Ak=']
-    print("unseal: ", client.unseal(keys))
+    client = VaultClient("localhost", 8200, token="s.G8b0bLPI35KA8yhBWCAfNgA5")
+    print("init: ", await client.initialize(1,1) )
+    keys =  ['ZNUv2gMf1sNz0QdnB6+z+buP2sYz53VcYsuax9rl9es='] 
+    print("unseal: ", await client.unseal(keys), client.keys)
     print("token: ", client.client.token)
+    await client.close()
 
 asyncio.run(init())
