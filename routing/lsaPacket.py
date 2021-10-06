04b1fa94e731188fa72daccf68d5e0fa035517b3
Lorenzo Pintore <s276315@studenti.polito.it>
2021-10-06 17:47:44 +0200

    new packet structure

--- routing/lsaPacket.py
+++ routing/lsaPacket.py
@@ -1,5 +1,5 @@
 import ipaddress
-
+import json
 dims = { 
     1 : {
         'version' : 1, 
@@ -29,6 +29,7 @@ dims = {
 }
 
 
+element_list = ["version", "type", "source", "routing", "neighbors", "timestamp"]
 
 class lsaPacket() :
     def __init__(self, version, nAdj, ptype = None, srcID = None, routingPort = None, routingIP = None, srcIP = None, srcPort = None, ids = None, costs = None, time = 0.0, options = None, auth = None): 
@@ -200,4 +201,28 @@ class lsaPacket() :
             s += f' \n | auth : {self.auth} | \n options : {self.options}' 
         return s 
         
-    
+class lsaPacketv2(): 
+    def __init__(self, data : dict = {}, json_size : int = None) -> int: 
+        self.data = data
+        self.json_size = json_size
+
+    def get_dimension(self) -> int: 
+        if self.json_size is None:  
+            self.json_size = len(json.dumps(self.data))
+
+        return self.json_size
+
+    def decode(self, rawbytes : bytearray) -> None: 
+        raw_string = rawbytes.decode()
+        self.data = json.loads(raw_string)
+
+    def encode(self) -> bytes: 
+        if all(key in self.data for key in element_list) : 
+            raw_string = json.dumps(self.data)
+            return raw_string.encode() 
+        else: 
+            return None
+
+    def __str__(self) -> str: 
+        s = f'version : {self.data["version"]} | type : {self.data["type"]}  | n_connected : {len(self.data["neighbors"])} | source = {self.data["source"]} | time = {self.data["timestamp"]} | neighbors : {self.data["neighbors"]} |'
+        return s
