d51dc16c0e9ea73e3f9c9566489a7bccdbc0be63
LorenzoPintore <lorenzo.pintore1@gmail.com>
2021-10-22 18:22:52 +0200

    updated controller added example secrets

--- kubernetes/controller/controller.py
+++ kubernetes/controller/controller.py
@@ -1,32 +1,76 @@
 import kopf 
 import logging
 import kubernetes 
+import base64
+import requests
+
+example_data = {
+    'keys' : [
+        {'key_ID' : 'exampleid1', 'key' : 'examplekey1'},
+        {'key_ID' : 'exampleid2', 'key' : 'examplekey2'}
+    ]
+}
 
 logger = logging.getLogger('controller')
+client_id = 'test'
+
+
 
 def login(username) -> str : 
+
+    api_instance = kubernetes.client.CoreV1Api()
+    credential_secret = api_instance.read_namespaced_secret(f"{username}-credentials", "default").data
+    username = base64.b64decode(credential_secret["username"])
+    password = base64.b64decode(credential_secret["password"])
+
+    client_secret = api_instance.read_namespaced_secret(f"qksclient-secret", "default").data
+    client_id = base64.b64decode(client_secret["client_id"])
+    client_secret = base64.b64decode(credential_secret["client_secret"])
+
+    header = {'Content-Type':'application/x-www-form-urlencoded'} 
+    data = f"client_id={client_id}&client_secret={client_secret}&grant_type=password&scope=openid&username={username}&password={password}"
+    try: 
+        x = requests.post('http://keycloak-service:8080/auth/realms/qks/protocol/openid-connect/token', data=data, headers=header)
+        token = x.json()['access_token']
     # perform login 
-    token = ""
+    except Exception as e: 
+        logger.error(f"Login error: impossible to authenticate user {username}")
+        token = None
     return token 
 
-def getKey(slave_SAE_ID, number, size) -> dict : 
-    # call getKey on the qks 
-    return {}
+def getKey(slave_SAE_ID:str, number:int, size:int, token:str) -> list : 
+    auth_header= {'Autorization' : f"Bearer {token}"}
+    # require keys calling getKey
+    req_data = {"size" : size, "number" : number}
+    req = requests.post(f'http://qks-service/api/v1/keys/{slave_SAE_ID}/enc_keys', json=req_data, headers=auth_header)
+    if req.status_code != 200 : 
+        return {"message" : "unable to retrieve key, QKS error"}
+    return req.json()['keys']
 
-def getKeyWithKeyIDs(master_SAE_ID:str, ids:list) -> dict : 
-    # call getKeyWithKeyIDs on the qks 
-    return {} 
+def getKeyWithKeyIDs(master_SAE_ID:str, ids:list, token:str) -> list : 
+    auth_header= {'Autorization' : f"Bearer {token}"}
+    # require keys calling getKeyWithKeyIDs
+    req_data = {"key_IDs" : ids}
+    req = requests.post(f'http://qks-service/api/v1/keys/{master_SAE_ID}/dec_keys', json=req_data, headers=auth_header)
+    if x.status_code != 200 : 
+        return {"message" : "unable to retrieve key. QKS error"}
+    return req.json()['keys'] 
 
-def createSecret(namespace: str, key_data: dict) -> str:
+def createSecret(namespace: str, key_data: dict, name:str) -> str:
     api_instance = kubernetes.client.CoreV1Api()
+    for key, val in key_data.items(): 
+        key_data[key] = base64.b64encode(val.encode()).decode()
     data = {'data' : key_data}
+    data['metadata'] = {'name' : name}
     return api_instance.create_namespaced_secret(namespace, data)
     
 
 @kopf.on.create('qks.controller', 'v1', 'keyrequests')
-def on_create(namespace, spec, body, **kwargs):
+def on_create(namespace, spec, body, name, **kwargs):
+    global logger 
     logger.warning(f"A key request object has been created: {body}")
 
+    cr_name = name
     try: 
         master_SAE_ID = spec['master_SAE_ID']
         slave_SAE_ID = spec['slave_SAE_ID']
@@ -35,32 +79,28 @@ def on_create(namespace, spec, body, **kwargs):
     number = spec['number'] if 'number' in spec else None 
     size = spec['size'] if 'size' in spec else None 
     ids = spec['ids'] if 'ids' in spec else None 
-
     
-    example_data = {
-        'keys' : [
-            {'key_ID' : 'exampleid1', 'key' : 'examplekey1'},
-            {'key_ID' : 'exampleid2', 'key' : 'examplekey2'}
-        ]
-    }
+    logger.info(f"master_SAE: {master_SAE_ID}, slave_SAE: {slave_SAE_ID}")
+    if ids is None: # getKey
+        logger.info(f"request for getKey - number: {number}, size: {size}")
+        token = login(master_SAE_ID) # authenticate retrieving master_SAE token from keycloak
+        if token is None: 
+            return {"message" : "ERROR in login"}
 
-    
-    print(f"master_SAE: {master_SAE_ID}, slave_SAE: {slave_SAE_ID}")
-    if ids is None: 
-        logger.warning(f"request for getKey - number: {number}, size: {size}")
-        token = login(master_SAE_ID)
-        # authenticate retrieving master_SAE token from keycloak
-        # require to the QKS the keys calling getKey
+        ret_data = getKey(slave_SAE_ID, number, size, token)
+
+    else: #getKeyWithKeyIDs
+        logger.info(f"request for a getKeyWithKeyIDs - IDs: {ids}")
+        token = login(slave_SAE_ID) # authenticate retrieving slave_SAE token from keycloak
+        if token is None: 
+            return {"message" : "ERROR in login"}
         
-    else: 
-        logger.warning(f"request for a getKeyWithKeyIDs - IDs: {ids}")
-        token = login(slave_SAE_ID)
-        # authenticate retrieving slave_SAE token from keycloak
-        # require to the QKS the keys calling getKeyWithKeyIDs
-        
-    ret_data = example_data 
-    secret_data = [{obj['key_ID'] : obj['key']} for obj in ret_data['keys']]
-    secret = createSecret(namespace, secret_data)
+        ret_data = getKeyWithKeyIDs(master_SAE_ID, ids, token)
+ 
+    secret_data = {}
+    for el in ret_data['keys']: 
+        secret_data[el['key_ID']] = el['key']
+    secret = createSecret(namespace, secret_data, cr_name)
     logger.warning(f"created secret with name: {secret.metadata.name}")
     return {"secret-name" : secret.metadata.name}
         
